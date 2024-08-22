0664dcd860b6e4c76483fa2e03b91f9137537bde
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-08-22 14:11:57 +0530

    Refactor App component to use Homepage and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import "./App.css";
+import Homepage from "./pages/homepage";
+import "./assets/global.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return <Homepage />;
 }
 
 export default App;
