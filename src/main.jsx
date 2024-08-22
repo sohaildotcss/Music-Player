0c49a8b413ea735860159547bb4fbd3ab5da676c
Shaik Sohail Ahamed <sohailqwerty7@gmail.com>
2024-08-22 20:51:06 +0530

    Refactor main.jsx to use ApolloClient for GraphQL API integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/global.css";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+  uri: "https://cms.samespace.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
